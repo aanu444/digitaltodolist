880fcf0a16df5e99137f3e5fcf7a2c4d49974ed2
Omolola Odunowo <omololaodunowo@gmail.com>
2021-02-25 14:04:40 +0100

    deploying todo app v2.0.0

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
-$(
-    $(".add").click(function(){
-        let inputVal= $("#todo").val();
-        let count=0;
-        if (inputVal != " "){
-        count++;
-        $(".container-0").css("display", "flex");
-        //$("#task").text(inputVal);
-        $(".todo-list").append('<li><div class="list"><input type="checkbox" id="check"'+ count
-         + ' name="check"><label for="check'+ count + '"></label><span id="task">'+ inputVal 
-         + '</span></div><span class="remove"><i class="material-icons">delete</i></span></li>')
 
-        }
-        $("#todo").val(" ")
-    }),
+//     $(".add").click(function(){
+//         let inputVal= $("#todo").val();
+//         let count=0;
+//         if (inputVal != " "){
+//         count++;
+//         $(".container-0").css("display", "flex");
+//         //$("#task").text(inputVal);
+//         $(".todo-list").append('<li><div class="list"><input type="checkbox" id="check"'+ count
+//          + ' name="check"><label for="check'+ count + '"></label><span id="task">'+ inputVal 
+//          + '</span></div><span class="remove"><i class="material-icons">delete</i></span></li>')
 
-    $("input[type='checkbox']").click(function(){
-        if($(this).is(":checked")){
-            alert("Are you done with this task?");
-        }
-    })
+//         }
+//         $("#todo").val(" ")
+//     });
 
-    // $(".remove").click(function(){
-    //     alert ("Are you sure you want to delete?")
-    // })
+//     $("#check").change(function(){
+//         alert("Yo");
+//         if($(this).is(":checked")){
+//             console.log("Are you done with this task?");
+//         }
+//         else if($(this).is(":not(:checked)")){
+//             console.log("This is not checked");
+//         }
+//     });
+
+//     $(".remove").click(function(){
+//         alert ("Are you sure you want to delete?")
+//     });
    
-)
+
+
+// if (typeof jQuery == 'undefined') {  
+//     console.log("jQuery is loaded");  
+//     //or
+//     alert("jQuery is loaded");  
+// } else {
+//     console.log("jQuery is not loaded");
+//     //or
+//      alert("jQuery is not loaded"); 
+// }
 
 
 
